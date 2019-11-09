e46d45a12a96dcad8e6b84cad46505fba65cf8fd
Manny-Santana <aresxwarlord@outlook.com>
2019-11-09 17:39:47 -0500

    added user posts and updated userHome page styling

--- controllers/user.js
+++ controllers/user.js
@@ -4,14 +4,13 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const PostModel = require("../models/post");
 
 //===============================
 // SHOW ROUTE
 //===============================
 
 router.get("/:id", (req, res) => {
-  //TODO: render the userHome.ejs and pass the current user's data to it. also set up this users route and controller in server js
-
   if (req.params.id === req.session.userID) {
     User.findById(req.session.userID, (err, foundUser) => {
       if (err) {
@@ -19,7 +18,6 @@ router.get("/:id", (req, res) => {
           '<h1> Could not find the User</h1> <a href="/" ><h2>Try Again </h2></a>'
         );
       } else {
-        console.log(foundUser.img);
         //redirect to self page view
         res.render("user/userHome.ejs", { User: foundUser });
       }
@@ -30,6 +28,42 @@ router.get("/:id", (req, res) => {
       //render stranger page view
       res.render("user/userPage.ejs", { User: foundUser });
     });
+  }
+});
+
+//===========================
+// POST ROUTES
+//===========================
+//
+//MAKE POST
+//FIXME: update this route to check if its a friends page and update to friends posts accordingly
+router.post("/:id/post", (req, res) => {
+  //first verfiy that the current user is posting on their own page and add to their posts
+  //otherwise add
+  // the userpage should pass the friends id
+  console.log("hit /user/id/post");
+  if (req.params.id === req.session.userID && req.session.loggedin === true) {
+    User.findById(req.session.userID, (err, User) => {
+      if (err) console.log(err.message);
+      //create post
+      const Post = {};
+      //pass in all requirements
+      Post.creatorID = User._id;
+      Post.creatorName = User.fName + " " + User.lName;
+      Post.creatorImg = User.img;
+      Post.content = req.body.post;
+      PostModel.create(Post, (err, post) => {
+        if (err) console.log(err.message);
+        console.log("created the post in the current users object");
+        console.log(post);
+        User.posts.push(post);
+        User.save();
+      });
+
+      res.redirect(`/user/${User._id}`);
+    });
+  } else {
+    res.send("posting to a friends page is currently in development");
   }
 });
 
